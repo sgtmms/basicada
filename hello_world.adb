328d093ee475b4442a8fa04532ad5212594abda4
sgtmms <sgtmms@gmail.com>
2022-11-13 21:20:35 -0600

    if

--- hello_world.adb
+++ hello_world.adb
@@ -2,7 +2,30 @@ with Ada.Text_IO;
 
 procedure Hello_World is
 begin
-   for i in 1..5 loop
-      Ada.Text_IO.Put_Line ("Hello Ada World!");
+
+   J : Integer := 7;
+   K : Integer := 7;
+   for I in 1..5 loop
+      Ada.Text_IO.Put_Line ("Hello Ada World!" & Integer'Image (I));
    end loop;
+
+   loop
+
+      if J mod 2 = 0 then
+         Put_Line ( Integer'Image (J))
+                & " is even");
+      else
+         Put_Line ( Integer'Image (J))
+                & " is odd");
+
+      --  Exit statement:
+      exit when J > 200;
+      --        ^ Boolean condition
+
+      --  Assignment:
+      J := J + K;
+    
+   end loop;
+
+
 end Hello_World;
